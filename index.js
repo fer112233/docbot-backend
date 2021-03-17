9db1fa356440026cf83ddab14044796a3a4732de
Fer <fpascualsoriano5@gmail.com>
2021-03-17 13:13:53 +0100

    Changes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const getRecords = (request, response) => {
 
 const addRecord = (request, response) => {
     const {id, timeOfMeasure, glucoseValue} = request.body
-    print(request.body)
+    console.log(request.body)
     pool.query(
         'INSERT INTO records (id, timeofmeasure, glucosevalue) VALUES ($1, $2, $3)',
         [id, timeOfMeasure, glucoseValue],
